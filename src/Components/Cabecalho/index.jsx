14a4aff5630a0452ebeeb9423cdfc78193bfc433
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-08-02 16:33:35 -0300

    conditional style of btn

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -9,12 +9,12 @@ const Cabecalho = () => {
     <Header className="cabecalho">
       <Image className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <Btn className="btn-secundario" href="https://google.com">
           Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </Btn>
+        <Btn primary className="btn-primario" href="https://google.com">
           Sair
-        </a>
+        </Btn>
       </div>
     </Header>
   );
@@ -34,4 +34,16 @@ const Header = styled.nav`
 const Image = styled.img`
   height: 50px;
   width: 50px;
+`;
+
+const Btn = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  color: ${(props) => (props.primary ? "white" : corPrimaria)};
+  background: ${(props) => (props.primary ? corPrimaria : "white")};
 `;
