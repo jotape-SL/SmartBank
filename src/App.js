ad5571c9e58d7839824a78dfe6117f7dfb5f7506
João Pedro de Sousa Cruz <jolunyna@gmail.com>
2022-08-03 18:04:16 -0300

    Dark Theme created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
+import { temaClaro, temaEscuro } from "./Components/UI/Temas";
 import GlobalStyle from "./Components/GlobalStyle";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaEscuro}>
       <GlobalStyle />
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -21,7 +21,7 @@ const Container = () => {
 export default Container;
 
 const ContainerWrapper = styled.div`
-  background-color: ${fundoClaro};
+  background-color: ${({ theme }) => theme.body};
   min-height: 90vh;
   padding: 0px 15vw;
 `;
@@ -30,6 +30,7 @@ const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  color: ${({ theme }) => theme.text};
 
   @media (max-width: 800px) {
     flex-direction: column;
